8bfb711f1a6c56184cb86915f9159806234720ea
5hev <s-h-e-v@mail.ru>
2025-02-24 04:26:02 +0300

    123

--- src/bot.js
+++ src/bot.js
@@ -27,7 +27,7 @@ class SwearBot {
     console.log(msg);
     console.log("===============  END  ================");
     if (msg.text && msg.reply_to_message && msg.reply_to_message.from.id == 7855984249) {
-      this.bot.sendMessage(msg.chat.id, sample(getReplies(msg.text)), { reply_to_message_id: msg.message_id, });
+      this.bot.sendMessage(msg.chat.id, sample(getReplies(msg.text)), { reply_to_message: msg.message_id, });
     } else {
       //console.log("============ error ============");
       //console.log(msg);
